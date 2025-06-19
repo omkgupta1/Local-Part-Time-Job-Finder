93915b45851dbcf2eac6d5a2616ff6e79fcaf686
Aayush Patel <AayushPatel@aayush-ka-MacBook-Air.local>
2025-06-19 21:34:12 +0530

    Initial push

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Login from './login.jsx'; // adjust path if needed
 function Home() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-900 text-white">
-      <h1 className="text-4xl font-bold mb-6">Hello World</h1>
+      <h1 className="text-4xl font-bold mb-6">Nitin bihari World</h1>
       <Link to="/login">
         <button className="bg-blue-600 hover:bg-blue-700 px-4 py-2 rounded text-white font-semibold">
           Login
